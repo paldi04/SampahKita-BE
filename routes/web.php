6f1e061b6cf3660ec1d1bdd211a46c1dd413e4ab
Muhammad Aditya Tisnadinata <tisnadinata@gmail.com>
2024-06-01 13:06:25 +0700

    add storage link route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('It Works!');
+});
+Route::get('/artisan/storage-link', function () {
+    $exitCode = Artisan::call('storage:link');
+    return 'Storage link created';
 });
