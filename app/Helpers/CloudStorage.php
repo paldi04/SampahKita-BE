6ebf1cafa0f27f2a584a46f80f581bce26e1e7a1
Muhammad Aditya Tisnadinata <tisnadinata@gmail.com>
2024-06-07 00:51:55 +0700

    bugfix cloud storage

--- app/Helpers/CloudStorage.php
+++ app/Helpers/CloudStorage.php
@@ -3,6 +3,11 @@ namespace App\Helpers;
 
 use Exception;
 use Google\Cloud\Storage\StorageClient;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class CloudStorage {
 
@@ -19,34 +24,93 @@ class CloudStorage {
     public static function uploadBase64Image($base64Image, $uploadPath)
     {
         self::init();
-        $dataUriScheme = explode(',', $base64Image)[0];
-        if (!in_array($dataUriScheme, ['data:image/png;base64', 'data:image/jpg;base64', 'data:image/jpeg;base64'])) {
-            throw new Exception('Invalid image format.');
+        if (!$tmpFileObject = self::validateBase64($base64Image, ['png', 'jpg', 'jpeg'])) {
+            return ['success' => false, 'error' => 'Invalid image format.'];
         }
     
-        $fileExtension = explode('/', explode(';', $dataUriScheme)[0])[1];
-        if (!in_array($fileExtension, ['png', 'jpg', 'jpeg'])) {
-            throw new Exception('Invalid image format.');
-        }
-        
-        $objectName = time() . '.' . $fileExtension;
+        $storedFilePath = self::storeTemporaryLocalFile($tmpFileObject, $uploadPath);
         try {
-            self::$cloudStorage->bucket(self::$bucketName)->upload($base64Image, [
-                'name' => $uploadPath . '/' . $objectName
+            self::$cloudStorage->bucket(self::$bucketName)->upload(Storage::disk('public')->get($storedFilePath), [
+                'name' => $storedFilePath
             ]);
-            
-            $object = self::$cloudStorage->bucket(self::$bucketName)->object($objectName);
-            $publicUrl = 'https://storage.googleapis.com/' . self::$bucketName . '/' . $uploadPath . '/' . $object->name();
+            Storage::disk('public')->delete($storedFilePath);
+
+            $publicUrl = 'https://storage.googleapis.com/' . self::$bucketName . '/' . $storedFilePath;
         
-            // $updateAccess = self::setPublicAccess($publicUrl);
-            // if (!$updateAccess['success']) {
-            //     return $updateAccess;
-            // }
+            $updateAccess = self::setPublicAccess($publicUrl);
+            if (!$updateAccess['success']) {
+                return $updateAccess;
+            }
             return ['success' => true, 'url' => $publicUrl];
         } catch (Exception $e) {
             return ['success' => false, 'error' => $e->getMessage()];
         }
     }
+    
+    public static function validateBase64(string $base64data, array $allowedMimeTypes)
+    {
+        // strip out data URI scheme information (see RFC 2397)
+        if (str_contains($base64data, ';base64')) {
+            list(, $base64data) = explode(';', $base64data);
+            list(, $base64data) = explode(',', $base64data);
+        }
+    
+        // strict mode filters for non-base64 alphabet characters
+        if (base64_decode($base64data, true) === false) {
+            return false;
+        }
+    
+        // decoding and then re-encoding should not change the data
+        if (base64_encode(base64_decode($base64data)) !== $base64data) {
+            return false;
+        }
+    
+        $fileBinaryData = base64_decode($base64data);
+    
+        // temporarily store the decoded data on the filesystem to be able to use it later on
+        $tmpFileName = tempnam(sys_get_temp_dir(), 'medialibrary');
+        file_put_contents($tmpFileName, $fileBinaryData);
+    
+        $tmpFileObject = new File($tmpFileName);
+    
+        // guard against invalid mime types
+        $allowedMimeTypes = Arr::flatten($allowedMimeTypes);
+    
+        // if there are no allowed mime types, then any type should be ok
+        if (empty($allowedMimeTypes)) {
+            return $tmpFileObject;
+        }
+    
+        // Check the mime types
+        $validation = Validator::make(
+            ['file' => $tmpFileObject],
+            ['file' => 'mimes:' . implode(',', $allowedMimeTypes)]
+        );
+    
+        if ($validation->fails()) {
+            return false;
+        }
+    
+        return $tmpFileObject;
+    }
+    
+    public static function storeTemporaryLocalFile(File $tmpFileObject, $uploadPath = 'uploadPath')
+    {
+        $tmpFileObjectPathName = $tmpFileObject->getPathname();
+    
+        $file = new UploadedFile(
+            $tmpFileObjectPathName,
+            $tmpFileObject->getFilename(),
+            $tmpFileObject->getMimeType(),
+            0,
+            true
+        );
+    
+        $storedFile = $file->store($uploadPath, ['disk' => 'public']);
+        unlink($tmpFileObjectPathName); // delete temp file
+        return $uploadPath . '/' . basename($storedFile);
+    }
+    
 
     public static function setPublicAccess($filePath)
     {
